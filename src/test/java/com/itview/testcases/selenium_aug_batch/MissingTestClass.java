3b4fbd5bb08eeed4a8f6de995bf975b8ff6963ce
AutomationTest001 <automationtest307@gmail.com>
2023-10-21 12:56:00 +0530

    added miising test cases

---
+++ src/test/java/com/itview/testcases/selenium_aug_batch/MissingTestClass.java
@@ -0,0 +1,11 @@
+package com.itview.testcases.selenium_aug_batch;
+
+public class MissingTestClass {
+
+	public static void main(String[] args) {
+
+     System.out.println("Missing requirement code !!!!!!!");
+
+	}
+
+}
